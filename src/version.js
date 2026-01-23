877887b32d3a2170bb7bc0c8f551a19e4f83b92a
Rustik <162493434+RustikArt@users.noreply.github.com>
2026-01-23 18:55:00 +0100

    version changer

--- src/version.js
+++ src/version.js
@@ -1,3 +1,2 @@
-export const WEBSITE_VERSION = "Version 5.0 ALPHA-VF5.1 EDITION";
-export const VERSION_DETAILS = "All fixs data set v1 | 500bugs";
-// commit text: 5.0 ALPHA-VF5.1 | All fixs data set v1 | 500bugs
+export const WEBSITE_VERSION = "Version 5.0 BETA EDITION";
+export const VERSION_DETAILS = "Vsc build";
