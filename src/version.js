f17b36c7ea50dd3f196fa0a55c8f30dd546fa21b
Rustik <162493434+RustikArt@users.noreply.github.com>
2026-01-23 17:39:44 +0100

    5.0 ALPHA-VF5.1 | All fixs data set v1 | 500bugs

--- src/version.js
+++ src/version.js
@@ -1,3 +1,3 @@
-export const WEBSITE_VERSION = "Version 5.0 ALPHA-VF5 EDITION";
-export const VERSION_DETAILS = "All fixs data set v1";
-// commit text: 5.0 ALPHA-VF5 | All fixs data set v1
+export const WEBSITE_VERSION = "Version 5.0 ALPHA-VF5.1 EDITION";
+export const VERSION_DETAILS = "All fixs data set v1 | 500bugs";
+// commit text: 5.0 ALPHA-VF5.1 | All fixs data set v1 | 500bugs
