d95711627349b86fc180233399d63bf3d93a9d7b
Rustik <162493434+RustikArt@users.noreply.github.com>
2026-02-01 11:06:46 +0100

    VercelFix

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3459,7 +3459,6 @@ const Dashboard = () => {
                                                             </div>
                                                         )}
                                                     </div>
-                                                </div>
                                             </div>
 
                                             <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '1rem' }}>
